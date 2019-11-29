04d371dd9d4ce882e3dee899fcff8202bb22e81b
Erik Mattfolk <erik.mattfolk@gmail.com>
2019-11-29 18:26:40 +0100

    Prepare C++ template

---
+++ 2019/template.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <fstream>
+#include "collections.h"
+
+using namespace std;
+using Input = string;
+using Answer = int;
+
+/*
+ * Read input from file.
+ */
+Input get_input(const char* filename) {
+    Input input;
+    ifstream is(filename);
+    is >> input;
+    is.close();
+
+    return input;
+}
+
+/*
+ * Solve the first problem.
+ */
+Answer solve_first(Input& input) {
+    Answer ans = 0;
+    return ans;
+}
+
+/*
+ * Solve the second problem.
+ */
+Answer solve_second(Input& input) {
+    Answer ans = 0;
+    return ans;
+}
+
+/*
+ * Run solutions.
+ */
+int main(int argc, char** argv) {
+
+    Input input = get_input(argv[1]);
+
+    auto start = chrono::high_resolution_clock::now();
+
+    Answer first_answer = solve_first(input);
+    Answer second_answer = solve_second(input);
+
+    auto end = chrono::high_resolution_clock::now();
+
+    cout << "First: " << first_answer << endl
+        << "Second: " << second_answer << endl
+        << "Time: " << chrono::duration_cast<chrono::milliseconds>(end-start).count()
+        << "ms" << endl;
+
+    return 0;
+}
