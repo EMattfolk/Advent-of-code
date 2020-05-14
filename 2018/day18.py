c317c00f0d5e262ce06807fd1c18da8314f43f8a
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-05-14 22:10:37 +0200

    Add execution to day 18

--- 2018/day18.py
+++ 2018/day18.py
@@ -23,31 +23,39 @@ def grid_str ():
     flat_grid = []
     for l in grid: flat_grid += l
     return "".join(flat_grid)
-    
-st = clock()
-s = grid_str()
-while s not in grid_set:
 
-    grid_set.add(s)
-    grids.append(s)
+def both():
 
-    new_grid = [[] for i in range(size)]
-    for x in range(size):
-        for y in range(size):
-            adj = get_adj(x, y)
-            new = grid[y][x]
-            if new == "." and adj.count("|") >= 3: new = "|"
-            elif new == "|" and adj.count("#") >= 3: new = "#" 
-            elif new == "#" and (adj.count("#") < 1 or adj.count("|") < 1): new = "." 
-            new_grid[y].append(new)
+    global grid
+    global grids
 
-    grid = new_grid
+    st = clock()
     s = grid_str()
+    while s not in grid_set:
 
-offset = grids.index(s)
-cycle_index = (goal - offset) % (len(grids) - offset)
+        grid_set.add(s)
+        grids.append(s)
 
-res1 = grids[10].count("#") * grids[10].count("|")
-res2 = grids[offset + cycle_index].count("#") * grids[offset + cycle_index].count("|")
+        new_grid = [[] for i in range(size)]
+        for x in range(size):
+            for y in range(size):
+                adj = get_adj(x, y)
+                new = grid[y][x]
+                if new == "." and adj.count("|") >= 3: new = "|"
+                elif new == "|" and adj.count("#") >= 3: new = "#"
+                elif new == "#" and (adj.count("#") < 1 or adj.count("|") < 1): new = "."
+                new_grid[y].append(new)
 
-print("First:", res1, "Second:", res2, "Time:", clock() - st)
+        grid = new_grid
+        s = grid_str()
+
+    offset = grids.index(s)
+    cycle_index = (goal - offset) % (len(grids) - offset)
+
+    res1 = grids[10].count("#") * grids[10].count("|")
+    res2 = grids[offset + cycle_index].count("#") * grids[offset + cycle_index].count("|")
+
+    print("First:", res1, "Second:", res2, "Time:", clock() - st)
+
+if __name__ == "__main__":
+    both()
