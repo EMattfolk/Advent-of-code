65f964b8a07c8d21b26fc0f36130488eb17ea0f6
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-05-14 21:22:02 +0200

    Make day 12 more robust

--- 2018/day12.py
+++ 2018/day12.py
@@ -4,26 +4,28 @@ from time import process_time as clock
 with open("12.txt") as f:
     data = [i.split() for i in f]
 cells = list(data[0][2])
-center = { i:j for i,j in zip([a[0] for a in data[1:]], [a[2] for a in data[1:]]) }
+transforms = { i:j for i,j in zip([a[0] for a in data[2:]], [a[2] for a in data[2:]]) }
 
 def solve (generations):
 
     cur_cells = cells.copy()
     cur_cells = list("." * 10) + cur_cells + list("." * 900)
 
+    old_state = ""
+
     for gen in range(generations):
 
         new_cells = cur_cells.copy() + ["."]
         current = "".join(cur_cells[:5])
-        changed = False
 
         for i in range(2, len(cur_cells) - 3):
-            if current in center:
-                if current != ".#..." and current != "..#..":
-                    changed = True
-                new_cells[i] = center[current]
+            if current in transforms:
+                new_cells[i] = transforms[current]
             current = current[1:] + cur_cells[i+3]
         cur_cells = new_cells
+
+        state = "".join(cur_cells).lstrip(".").rstrip(".")
+        changed, old_state = state != old_state, state
 
         if not changed: break
 
@@ -35,7 +37,7 @@ def solve (generations):
     for i, pot in enumerate(cur_cells):
         if cur_cells[i] == "#":
             res += i + offset
-        
+
     return res
 
 
@@ -43,7 +45,7 @@ def solve (generations):
 def first ():
     st = clock()
     res = solve(20)
-    print("First:", res, "Time:", clock() - st)
+    print("First:", res, "Time:", clock() - st, flush=True)
 
 # Function for solving the second problem
 def second ():
