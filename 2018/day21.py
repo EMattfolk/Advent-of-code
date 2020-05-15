a9b50572e7ee51f8538a88e11b5db0f948963c25
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-05-15 10:21:33 +0200

    Fix input on day 21

--- 2018/day21.py
+++ 2018/day21.py
@@ -1,6 +1,10 @@
 from time import process_time as clock
 
 """
+
+This problem has different registers depending on the solver.
+Only 3 registers has to be changed for any specific user.
+
 0 1 2 3 4 5
 a b c d e f
 
@@ -44,62 +48,10 @@ else:
         goto Loop3
 """
 
-def addr (reg, a, b, c):
-    reg[c] = reg[a] + reg[b]
-
-def addi (reg, a, b, c):
-    reg[c] = reg[a] + b
-
-def mulr (reg, a, b, c):
-    reg[c] = reg[a] * reg[b]
-
-def muli (reg, a, b, c):
-    reg[c] = reg[a] * b
-
-def banr (reg, a, b, c):
-    reg[c] = reg[a] & reg[b]
-
-def bani (reg, a, b, c):
-    reg[c] = reg[a] & b
-
-def borr (reg, a, b, c):
-    reg[c] = reg[a] | reg[b]
-
-def bori (reg, a, b, c):
-    reg[c] = reg[a] | b
-
-def setr (reg, a, b, c):
-    reg[c] = reg[a]
-
-def seti (reg, a, b, c):
-    reg[c] = a
-
-def gtir (reg, a, b, c):
-    reg[c] = int(a > reg[b])
-
-def gtri (reg, a, b, c):
-    reg[c] = int(reg[a] > b)
-
-def gtrr (reg, a, b, c):
-    reg[c] = int(reg[a] > reg[b])
-
-def eqir (reg, a, b, c):
-    reg[c] = int(a == reg[b])
-
-def eqri (reg, a, b, c):
-    reg[c] = int(reg[a] == b)
-
-def eqrr (reg, a, b, c):
-    reg[c] = int(reg[a] == reg[b])
-
-instruction_names = ["addr", "addi", "mulr", "muli", "banr", "bani", "borr", "bori", "setr", "seti", "gtir", "gtri", "gtrr", "eqir", "eqri", "eqrr"]
-instructions = [addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr]
-
 # Initialize the data
+# Not the most robust solution, but should work in most cases
 with open("21.txt") as f:
-    data = [l.strip() for l in f]
-    instr_reg = int(data[0][-1])
-    instr_list = [(instruction_names.index(l.split()[0]),) + tuple(int(n) for n in l.split()[1:]) for l in data[1:]]
+    initial_value = max(int(l.split()[1]) for l in f)
 
 # Function for solving the first problem
 def first ():
@@ -107,7 +59,7 @@ def first ():
     a, b, c, d, e, f = [0] * 6
 
     d = 65536 | e
-    e = 12670166
+    e = initial_value
 
     while True:
 
@@ -135,7 +87,7 @@ def second ():
     while True:
 
         d = 65536 | e
-        e = 12670166
+        e = initial_value
 
         while True:
 
