0224377c86b88308cd0c7e142519f4a093386490
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-05-26 18:16:26 +0200

    Do day 17

--- 2015/day17.c
+++ 2015/day17.c
@@ -2,6 +2,54 @@
 #include "string.h"
 
 char* solve_day_17(char* input) {
-    input[0] = '\0';
+
+    int containers[30];
+    int container_count = 0;
+
+    char* tok = strtok(input, "\n");
+
+    while (tok != NULL) {
+
+        containers[container_count++] = atoi(tok);
+
+        tok = strtok(NULL, "\n");
+    }
+
+    int cmp(const void* a, const void* b) {
+        return -(*(int*)a - *(int*)b);
+    }
+
+    qsort(containers, container_count, sizeof(int), cmp);
+
+    int combinations1 = 0;
+    int combinations2 = 0;
+    int min_containers = container_count;
+
+    void test_combinations(int eggnog, int cur_containers, int index) {
+        if (eggnog == 0) {
+            combinations1++;
+
+            if (cur_containers < min_containers) {
+                min_containers = cur_containers;
+                combinations2 = 1;
+            } else if (cur_containers == min_containers) {
+                combinations2++;
+            }
+
+            return;
+        }
+
+        if (eggnog < 0 || index == container_count) {
+            return;
+        }
+
+        test_combinations(eggnog - containers[index], cur_containers + 1, index + 1);
+        test_combinations(eggnog, cur_containers, index + 1);
+    }
+
+    test_combinations(150, 0, 0);
+
+    sprintf(input, "%d, %d", combinations1, combinations2);
+
     return input;
 }
