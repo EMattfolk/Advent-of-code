a76d2bb134dbd155f12386626c84a5475ee88fde
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-06-08 19:10:51 +0200

    Do day 5

--- 2016/src/day05.go
+++ 2016/src/day05.go
@@ -2,11 +2,39 @@ package src
 
 import (
     "fmt"
+    "crypto/md5"
+    "strconv"
+    "strings"
 )
 
 func Solve05(input string) string {
-    var ans1 int
-    var ans2 int
+    var ans1 string
+    var ans2 string
 
-    return fmt.Sprintf("%d, %d", ans1, ans2)
+    passw2 := [8]string{"", "", "", "", "", "", "", ""}
+    passw2_remaining := 8
+
+    i := 0
+    for passw2_remaining > 0 {
+        hash := md5.Sum([]byte(input + strconv.Itoa(i)))
+
+        if hash[0] == 0 && hash[1] == 0 && (hash[2] & 0xf0) == 0 {
+            if len(ans1) < 8 {
+                ans1 += fmt.Sprintf("%x", hash[2])
+            }
+
+            pos := hash[2]
+            char := fmt.Sprintf("%x", (hash[3] & 0xf0) >> 4)
+            if pos < 8 && passw2[pos] == "" {
+                passw2[pos] = char
+                passw2_remaining--
+            }
+        }
+
+        i++
+    }
+
+    ans2 = strings.Join(passw2[:], "")
+
+    return fmt.Sprintf("%s, %s", ans1, ans2)
 }
