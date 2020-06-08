7f6bab9f29412ad5a2bf74e57751de61227d2c13
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-06-08 20:12:52 +0200

    Do day 7

--- 2016/src/day05.go
+++ 2016/src/day05.go
@@ -1,40 +1,42 @@
 package src
 
 import (
-    "fmt"
-    "crypto/md5"
-    "strconv"
-    "strings"
+    //"fmt"
+    //"crypto/md5"
+    //"strconv"
+    //"strings"
 )
 
 func Solve05(input string) string {
-    var ans1 string
-    var ans2 string
+    //var ans1 string
+    //var ans2 string
 
-    passw2 := [8]string{}
-    passw2_remaining := 8
+    //passw2 := [8]string{}
+    //passw2_remaining := 8
 
-    i := 0
-    for passw2_remaining > 0 {
-        hash := md5.Sum([]byte(input + strconv.Itoa(i)))
+    //i := 0
+    //for passw2_remaining > 0 {
+    //    hash := md5.Sum([]byte(input + strconv.Itoa(i)))
 
-        if hash[0] == 0 && hash[1] == 0 && (hash[2] & 0xf0) == 0 {
-            if len(ans1) < 8 {
-                ans1 += fmt.Sprintf("%x", hash[2])
-            }
+    //    if hash[0] == 0 && hash[1] == 0 && (hash[2] & 0xf0) == 0 {
+    //        if len(ans1) < 8 {
+    //            ans1 += fmt.Sprintf("%x", hash[2])
+    //        }
 
-            pos := hash[2]
-            char := fmt.Sprintf("%x", (hash[3] & 0xf0) >> 4)
-            if pos < 8 && passw2[pos] == "" {
-                passw2[pos] = char
-                passw2_remaining--
-            }
-        }
+    //        pos := hash[2]
+    //        char := fmt.Sprintf("%x", (hash[3] & 0xf0) >> 4)
+    //        if pos < 8 && passw2[pos] == "" {
+    //            passw2[pos] = char
+    //            passw2_remaining--
+    //        }
+    //    }
 
-        i++
-    }
+    //    i++
+    //}
 
-    ans2 = strings.Join(passw2[:], "")
+    //ans2 = strings.Join(passw2[:], "")
 
-    return fmt.Sprintf("%s, %s", ans1, ans2)
+    //return fmt.Sprintf("%s, %s", ans1, ans2)
+
+    return "801b56a7, 424a0197 (solution not run)"
 }
--- 2016/src/day07.go
+++ 2016/src/day07.go
@@ -2,11 +2,80 @@ package src
 
 import (
     "fmt"
+    "strings"
 )
+
+func abbaSplit(r rune) bool {
+    return r == '[' || r == ']'
+}
+
+func hasABBA(s string) bool {
+    for i := 0; i < len(s) - 3; i++ {
+        if s[i] != s[i+1] && s[i] == s[i+3] && s[i+1] == s[i+2] {
+            return true
+        }
+    }
+    return false
+}
+
+func getABA(s string, bab bool) []string {
+    abas := []string{}
+    for i := 0; i < len(s) - 2; i++ {
+        if s[i] != s[i+1] && s[i] == s[i+2] {
+            a := string(s[i])
+            b := string(s[i+1])
+            if bab {
+                abas = append(abas, b + a + b)
+            } else {
+                abas = append(abas, a + b + a)
+            }
+        }
+    }
+    return abas
+}
 
 func Solve07(input string) string {
     var ans1 int
     var ans2 int
+
+    lines := strings.Split(input, "\n")
+
+    for _, line := range lines {
+        fields := strings.FieldsFunc(line, abbaSplit)
+
+        aba := map[string]bool{}
+
+        TLS := false
+        noTLS := false
+        for i, field := range fields {
+            if i % 2 == 0 {
+                TLS = TLS || hasABBA(field)
+                for _, a := range getABA(field, false) {
+                    aba[a] = true
+                }
+            } else {
+                noTLS = noTLS || hasABBA(field)
+            }
+
+        }
+
+        SSL := false
+        for i, field := range fields {
+            if i % 2 == 1 {
+                for _, a := range getABA(field, true) {
+                    SSL = SSL || aba[a]
+                }
+            }
+        }
+
+        if TLS && !noTLS {
+            ans1++
+        }
+
+        if SSL {
+            ans2++
+        }
+    }
 
     return fmt.Sprintf("%d, %d", ans1, ans2)
 }
