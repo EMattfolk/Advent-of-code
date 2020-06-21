bcddd933f11fef49202366488d29d2c39f46f618
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-06-21 17:29:41 +0200

    Do day 5

--- 2017/src/day05.js
+++ 2017/src/day05.js
@@ -1,5 +1,31 @@
 export default function solve05(input) {
     let ans1 = 0;
     let ans2 = 0;
-    return [];
+
+    let maze = input.split("\n").map((line) => parseInt(line));
+    let mazeCopy = maze.slice();
+    let index = 0;
+
+    while (index >= 0 && index < maze.length) {
+        index += maze[index]++;
+        ans1++;
+    }
+
+    maze = mazeCopy;
+
+    index = 0;
+    while (index >= 0 && index < maze.length) {
+        let oldIndex = index;
+        index += maze[index];
+
+        if (maze[oldIndex] >= 3) {
+            maze[oldIndex]--;
+        } else {
+            maze[oldIndex]++;
+        }
+
+        ans2++;
+    }
+
+    return [ans1, ans2];
 }
