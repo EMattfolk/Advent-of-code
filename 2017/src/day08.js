cb748bbd2f79be68371ed2aa9cc267b16f616aff
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-06-22 17:57:38 +0200

    Do day 8

--- 2017/src/day08.js
+++ 2017/src/day08.js
@@ -1,5 +1,42 @@
 export default function solve08(input) {
     let ans1 = 0;
     let ans2 = 0;
-    return [];
+
+    let ops = new Map([
+        [">", (a, b) => a > b],
+        ["<", (a, b) => a < b],
+        [">=", (a, b) => a >= b],
+        ["<=", (a, b) => a <= b],
+        ["==", (a, b) => a == b],
+        ["!=", (a, b) => a != b],
+    ]);
+
+    let vars = new Map();
+
+    input.split("\n").forEach((line) => {
+        let instruction = line.split(" ");
+
+        let reg1 = instruction[0];
+        let reg2 = instruction[4];
+
+        if (!vars.has(reg1)) {
+            vars.set(reg1, 0);
+        }
+        if (!vars.has(reg2)) {
+            vars.set(reg2, 0);
+        }
+
+        let dir = instruction[1] === "inc" ? 1 : -1;
+        let op = ops.get(instruction[5]);
+
+        let change = parseInt(instruction[2]) * dir;
+        if (op(vars.get(reg2), parseInt(instruction[6]))) {
+            vars.set(reg1, vars.get(reg1) + change);
+            ans2 = Math.max(vars.get(reg1), ans2);
+        }
+    });
+
+    ans1 = Array.from(vars.values()).reduce((a, b) => Math.max(a, b), 0);
+
+    return [ans1, ans2];
 }
