dc467afd1424f7df8dbbc79cd00d41499d5d0d8e
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-06-22 20:19:04 +0200

    Do day 9

--- 2017/src/day09.js
+++ 2017/src/day09.js
@@ -1,5 +1,46 @@
 export default function solve09(input) {
     let ans1 = 0;
     let ans2 = 0;
-    return [];
+
+    function parseGroup(index, depth) {
+        ans1 += depth;
+        index = parseGroupContent(index + 1, depth + 1);
+        return index + 1;
+    }
+
+    function parseGarbage(index) {
+        ans2--;
+        while (input[index] != ">") {
+            if (input[index] == "!") {
+                index++;
+                ans2--;
+            }
+            index++;
+            ans2++;
+        }
+
+        return index + 1;
+    }
+
+    function parseGroupContent(index, depth) {
+        while (input[index] != "}") {
+            if (input[index] == ",") {
+                index++;
+            }
+
+            if (input[index] == "{") {
+                index = parseGroup(index, depth);
+            } else if (input[index] == "<") {
+                index = parseGarbage(index);
+            } else {
+                console.log(index);
+            }
+        }
+
+        return index;
+    }
+
+    parseGroup(0, 1);
+
+    return [ans1, ans2];
 }
