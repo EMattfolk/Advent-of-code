225eed16c72b37716d46d4caae4f652cd103d314
Erik Mattfolk <erik.mattfolk@gmail.com>
2020-12-01 21:16:06 +0100

    Add timing and optimize day 1

--- 2020/src/day01.rs
+++ 2020/src/day01.rs
@@ -1,18 +1,28 @@
+use std::iter::FromIterator;
+use std::collections::HashSet;
 pub fn solve(input: String) -> String {
-    let numbers: Vec<u32> = input.split_whitespace().map(|x| x.parse().unwrap()).collect();
+    let nums: Vec<u32> = input.split_whitespace().map(|x| x.parse().unwrap()).collect();
+    let num_set: HashSet<u32> = HashSet::from_iter(nums.clone());
 
     let mut ans1 = 0;
     let mut ans2 = 0;
-    for i in 0..numbers.len() {
-        for j in i+1..numbers.len() {
-            if numbers[i] + numbers[j] == 2020 {
-                ans1 = numbers[i] * numbers[j];
+    for i in 0..nums.len() {
+        if nums[i] > 2020 {
+            continue;
+        }
+        for j in i+1..nums.len() {
+            if nums[i] + nums[j] > 2020 {
+                continue;
             }
-            for k in j+1..numbers.len() {
-                if numbers[i] + numbers[j] + numbers[k] == 2020 {
-                    ans2 = numbers[i] * numbers[j] * numbers[k];
-                }
+            if nums[i] + nums[j] == 2020 {
+                ans1 = nums[i] * nums[j];
             }
+            if num_set.contains(&(2020 - nums[i] - nums[j])) {
+                ans2 = nums[i] * nums[j] * (2020 - nums[i] - nums[j]);
+            }
+        }
+        if ans1 > 0 && ans2 > 0 {
+            break;
         }
     }
 
--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -1,9 +1,24 @@
 use std::fs;
+use std::time::SystemTime;
 
 mod day01;
 
 fn main() {
-    let day_input = fs::read_to_string("input/day01.txt").unwrap().to_string();
 
-    println!("{}", day01::solve(day_input));
+    let days = 1;
+
+    let day_fns = [day01::solve];
+
+    for i in 0..days {
+        let day = i + 1;
+        let day_input = fs::read_to_string(format!("input/day{:02}.txt", day))
+            .unwrap()
+            .to_string();
+
+        let st = SystemTime::now();
+        let ans = day_fns[i](day_input);
+        let elapsed = st.elapsed().unwrap().as_nanos() as f64 / 1000000.0;
+
+        println!("Day {:>2}: {:.3}ms - {}", day, elapsed, ans);
+    }
 }
