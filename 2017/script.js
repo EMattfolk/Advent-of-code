f17bc34be1ad1b317438e30753481e82df13e02a
Erik Mattfolk <erik.mattfolk@gmail.com>
2021-08-19 14:56:59 +0200

    Run 2017 in node

--- 2017/script.js
+++ 2017/script.js
@@ -1,36 +1,63 @@
-import { solutions } from "/src/solutions.js";
+import { solutions } from "./src/solutions.js";
 
-let button = document.getElementById("runButton");
-button.addEventListener("click", () => {
-    let results = document.getElementById("results");
-    results.innerHTML = "";
+const browser = typeof document !== "undefined";
 
-    solutions.forEach(async function(fn, i) {
-        let day = i + 1;
+let _performance = typeof performance === "undefined" ? undefined : performance;
+
+if (!browser) {
+    (async () => {
+        _performance = (await import("perf_hooks")).performance;
+    })();
+}
+
+const runAll = async () => {
+    if (browser) {
+        document.getElementById("results").innerHTML = "";
+    }
+
+    for (let day = 1; day <= solutions.length; day++) {
         let elapsed = 0;
-        let ans = await fetch("input/" + (day < 10 ? "0" : "") + day + ".txt")
-            .then(response => response.text())
-            .then(text => {
-                elapsed = performance.now();
-                let ans = fn(day !== 19 ? text.trim() : text)
-                elapsed = performance.now() - elapsed;
-                return ans;
-            });
+        let inputFile = "input/" + ("0" + day).slice(-2) + ".txt";
+        let input = browser ?
+            await fetch(inputFile).then(response => response.text())
+            :
+            (await import("fs")).readFileSync(inputFile).toString();
+
+        elapsed = _performance.now();
+        let ans = solutions[day - 1](day !== 19 ? input.trim() : input)
+        elapsed = _performance.now() - elapsed;
 
         if (ans.length === 0) {
             return;
         }
 
-        let ansEntry = document.createElement("pre");
-        ansEntry.innerHTML += (day < 10 ? " " : "") + day + ": ";
-        ansEntry.innerHTML += ("   " + elapsed.toFixed(3)).slice(-8) + "ms ";
-        ansEntry.innerHTML += "<code id=\"first\">" + ans[0] + "</code>";
-        if (ans.length == 2) {
-            ansEntry.innerHTML += " <code id=\"second\">" + ans[1] + "</code>";
+        let text = (" " + day).slice(-2) + ": "
+                 + ("   " + elapsed.toFixed(3)).slice(-8) + "ms ";
+
+        let ansEntry = undefined;
+        if (browser) {
+            ansEntry = document.createElement("pre");
+            ansEntry.innerHTML = text + "<code id=\"first\">" + ans[0] + "</code>";
+            if (ans.length == 2) {
+                ansEntry.innerHTML += " <code id=\"second\">" + ans[1] + "</code>";
+            }
+        } else {
+            text += ans[0] + (ans[1] ? ", " + ans[1] : "");
         }
 
-        let listEntry = document.createElement("li");
-        listEntry.appendChild(ansEntry);
-        results.appendChild(listEntry);
-    })
-});
+        if (browser) {
+            let listEntry = document.createElement("li");
+            listEntry.appendChild(ansEntry);
+            document.getElementById("results").appendChild(listEntry);
+        } else {
+            console.log(text);
+        }
+    }
+}
+
+if (browser) {
+    document.getElementById("runButton")
+            .addEventListener("click", runAll);
+} else {
+    runAll()
+}
