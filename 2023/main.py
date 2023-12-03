347cdd41c46d24802e6a9b1e10911c59150e167e
Erik Mattfolk <erik.mattfolk@gmail.com>
2023-12-03 21:06:15 +0100

    Auto import + nicer format

--- 2023/main.py
+++ 2023/main.py
@@ -1,7 +1,13 @@
-import day01
-import day02
-import day03
+from importlib import import_module
+from time import process_time
 
-print(day01.solve(open("input/day01.txt").read().rstrip()))
-print(day02.solve(open("input/day02.txt").read().rstrip()))
-print(day03.solve(open("input/day03.txt").read().rstrip()))
+for day in range(1, 26):
+    day_string = f"day{day:>02}"
+    try:
+        m = import_module(day_string)
+    except:
+        break
+    st = process_time()
+    res = m.solve(open("input/" + day_string + ".txt").read().rstrip())
+    end = process_time()
+    print(f"Day {day:>2}: {(end-st)*1000:>8.3f}ms - {res[0]}, {res[1]}")
