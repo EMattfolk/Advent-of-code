fdc17948ea93e0d306bcd97190775d8210c2d9ba
Erik Mattfolk <erik.mattfolk@gmail.com>
2024-12-03 20:53:43 +0100

    Parallel runner

--- 2024/lib/main.erl
+++ 2024/lib/main.erl
@@ -2,14 +2,24 @@
 
 -export([ run/0, read_input/1 ]).
 
+day_string(Day) ->
+    iolist_to_binary(["day", string:pad(integer_to_binary(Day), 2, leading, ~"0")]).
+
 read_input(Day) -> 
-    Filename = [~"input/day", string:pad(integer_to_binary(Day), 2, leading, ~"0"), ~".txt"],
+    Filename = [~"input/", day_string(Day), ~".txt"],
     {ok, Res} = file:read_file(iolist_to_binary(Filename)),
     Res.
 
 
 run() ->
-    [ day01:solve(read_input(1))
-    , day02:solve(read_input(2))
-    , day03:solve(read_input(3))
-    ].
+    Days = [1, 2, 3],
+    Runner = self(),
+    lists:map(fun (Day) ->
+                      Module = binary_to_atom(day_string(Day)),
+                      spawn(fun() -> Runner ! {Day, Module:solve(read_input(Day))} end)
+              end, Days),
+    lists:map(fun (Day) ->
+                      receive
+                          {Day, Res} -> Res
+                      end
+              end, Days).
