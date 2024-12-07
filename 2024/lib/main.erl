2b27e402f5ea79fc8b752855c067ef2b96efab41
Erik Mattfolk <erik.mattfolk@gmail.com>
2024-12-07 08:46:23 +0100

    Better runner

--- 2024/lib/main.erl
+++ 2024/lib/main.erl
@@ -1,6 +1,6 @@
 -module(main).
 
--export([ run/0, read_input/1 ]).
+-export([ run/0 ]).
 
 day_string(Day) ->
     iolist_to_binary(["day", string:pad(integer_to_binary(Day), 2, leading, ~"0")]).
@@ -13,19 +13,23 @@ read_input(Day) ->
 
 run() ->
     {ok, Inputs} = file:list_dir(~"input"),
-    Days = lists:seq(1, length(Inputs)),
+    Last = length(Inputs),
+    Days = lists:seq(1, Last),
     Runner = self(),
-    lists:map(fun (Day) ->
+    lists:foreach(fun (Day) ->
                       Module = binary_to_atom(day_string(Day)),
                       spawn(fun() ->
                                     Start = os:system_time(microsecond),
                                     Runner ! {Day, catch Module:solve(read_input(Day)), os:system_time(microsecond) - Start}
-                            end)
+                            end),
+                      io:format("Day ~2..0B:          ms - ..., ...\n", [Day])
               end, Days),
-    lists:foreach(fun (Day) ->
+    lists:foreach(fun (_) ->
                       {{Part1, Part2}, Elapsed} =
                           receive
-                              {D, Res, E} when D == Day -> {Res, E}
+                              {Day, Res, E} -> {Res, E}
                           end,
-                      io:format("Day ~2..0B: ~8.3f ms - ~p, ~p\n", [Day, Elapsed / 1000, Part1, Part2 ])
+                      io:put_chars(lists:duplicate(Last - Day + 1, "\e[1A")),
+                      io:format("Day ~2..0B: ~8.3f ms - ~p, ~p\r", [Day, Elapsed / 1000, Part1, Part2 ]),
+                      io:put_chars(lists:duplicate(Last - Day + 1, "\e[1B"))
               end, Days).
