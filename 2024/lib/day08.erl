671cb95fb0879245ffad5a2a4bde2a9c87184e84
Erik Mattfolk <erik.mattfolk@gmail.com>
2024-12-09 07:48:16 +0100

    Lists

--- 2024/lib/day08.erl
+++ 2024/lib/day08.erl
@@ -7,18 +7,18 @@
 pairs([X | Rest]) -> [{X, Y} || Y <- Rest] ++ pairs(Rest);
 pairs([]) -> [].
 
-place_antinodes(Beacons) -> lists:foldl(fun place_antinodes/2, #{}, maps:values(Beacons)).
+place_antinodes(Beacons) -> lists:uniq(lists:filter(fun in_bounds/1, lists:foldl(fun place_antinodes/2, [], maps:values(Beacons)))).
 place_antinodes(Nodes, Antinodes) -> 
     lists:foldl(
       fun ({A, B}, Acc) ->
               Diff = p:sub(B, A),
               P1 = p:sub(A, Diff),
               P2 = p:add(A, p:mul(Diff, 2)),
-              sets:add_element(P1, sets:add_element(P2, Acc))
+              [P1, P2 | Acc]
       end,
       Antinodes, pairs(Nodes)).
 
-place_antinodes2(Beacons) -> sets:from_list(lists:foldl(fun place_antinodes2/2, [], maps:values(Beacons))).
+place_antinodes2(Beacons) -> lists:uniq(lists:foldl(fun place_antinodes2/2, [], maps:values(Beacons))).
 place_antinodes2(Nodes, Antinodes) -> 
     lists:foldl(
       fun ({A, B}, Acc) ->
@@ -38,6 +38,8 @@ in_bounds({X, Y}) -> 0 =< X andalso X < ?Size andalso 0 =< Y andalso Y < ?Size.
 solve(Input) ->
     Lines = utils:lines(Input),
     Beacons = utils:group([ {C, {X, Y}} || {Y, L} <- lists:enumerate(0, Lines), {X, C} <- lists:enumerate(0, binary_to_list(L)), C /= $.]),
-    { sets:size(sets:filter(fun in_bounds/1, place_antinodes(Beacons)))
-    , sets:size(place_antinodes2(Beacons))
+    { length(place_antinodes(Beacons))
+    , length(place_antinodes2(Beacons))
     }.
+
+
