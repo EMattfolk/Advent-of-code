88995f4ef5d02c2c91518196c2ace2803709a861
Erik Mattfolk <erik.mattfolk@gmail.com>
2024-12-11 22:41:41 +0100

    Format main and utils using erlfmt

--- 2024/lib/main.erl
+++ 2024/lib/main.erl
@@ -1,15 +1,14 @@
 -module(main).
 
--export([ run/0, run/1 ]).
+-export([run/0, run/1]).
 
 day_string(Day) ->
     iolist_to_binary(["day", string:pad(integer_to_binary(Day), 2, leading, ~"0")]).
 
-read_input(Day) -> 
+read_input(Day) ->
     Filename = [~"input/", day_string(Day), ~".txt"],
     {ok, Res} = file:read_file(iolist_to_binary(Filename)),
     Res.
-
 
 run(Day) ->
     Module = binary_to_atom(day_string(Day)),
@@ -20,20 +19,27 @@ run() ->
     Last = length(Inputs),
     Days = lists:seq(1, Last),
     Runner = self(),
-    lists:foreach(fun (Day) ->
-                      Module = binary_to_atom(day_string(Day)),
-                      spawn(fun() ->
-                                    Start = os:system_time(microsecond),
-                                    Runner ! {Day, catch Module:solve(read_input(Day)), os:system_time(microsecond) - Start}
-                            end),
-                      io:format("Day ~2..0B:      ... ms - ..., ...\n", [Day])
-              end, Days),
-    lists:foreach(fun (_) ->
-                      {{Part1, Part2}, Elapsed} =
-                          receive
-                              {Day, Res, E} -> {Res, E}
-                          end,
-                      io:put_chars(lists:duplicate(Last - Day + 1, "\e[1A")),
-                      io:format("Day ~2..0B: ~8.3f ms - ~p, ~p\r", [Day, Elapsed / 1000, Part1, Part2 ]),
-                      io:put_chars(lists:duplicate(Last - Day + 1, "\e[1B"))
-              end, Days).
+    lists:foreach(
+        fun(Day) ->
+            Module = binary_to_atom(day_string(Day)),
+            spawn(fun() ->
+                Start = os:system_time(microsecond),
+                Runner !
+                    {Day, catch Module:solve(read_input(Day)), os:system_time(microsecond) - Start}
+            end),
+            io:format("Day ~2..0B:      ... ms - ..., ...\n", [Day])
+        end,
+        Days
+    ),
+    lists:foreach(
+        fun(_) ->
+            {{Part1, Part2}, Elapsed} =
+                receive
+                    {Day, Res, E} -> {Res, E}
+                end,
+            io:put_chars(lists:duplicate(Last - Day + 1, "\e[1A")),
+            io:format("Day ~2..0B: ~8.3f ms - ~p, ~p\r", [Day, Elapsed / 1000, Part1, Part2]),
+            io:put_chars(lists:duplicate(Last - Day + 1, "\e[1B"))
+        end,
+        Days
+    ).
--- 2024/lib/utils.erl
+++ 2024/lib/utils.erl
@@ -1,7 +1,15 @@
 -module(utils).
 
--export([ ints/1, ints/2, lines/1, sections/1, grid/1, par/2, group/1, apply_n/3 ]).
-
+-export([
+    ints/1,
+    ints/2,
+    lines/1,
+    sections/1,
+    grid/1,
+    par/2,
+    group/1,
+    apply_n/3
+]).
 
 -doc """
 Split a string into a list of integers.
@@ -10,16 +18,26 @@ ints(S) -> ints(S, "|, \n").
 ints(S, Sep) -> lists:map(fun binary_to_integer/1, string:lexemes(S, Sep)).
 lines(S) -> string:lexemes(S, "\n").
 sections(S) -> string:split(S, "\n\n").
-grid(S) -> [{{X, Y}, C} || {Y, L} <- lists:enumerate(0, lines(S)), {X, C} <- lists:enumerate(0, binary_to_list(L))].
+grid(S) ->
+    [
+        {{X, Y}, C}
+     || {Y, L} <- lists:enumerate(0, lines(S)), {X, C} <- lists:enumerate(0, binary_to_list(L))
+    ].
 par(F, L) ->
     Parent = self(),
     Ref = make_ref(),
     lists:foreach(fun(V) -> spawn(fun() -> Parent ! {Ref, F(V)} end) end, L),
-    lists:map(fun (_) -> receive {Ref, V} -> V end end, L).
+    lists:map(
+        fun(_) ->
+            receive
+                {Ref, V} -> V
+            end
+        end,
+        L
+    ).
 
 group([]) -> #{};
-group([{A, B} | Rest]) ->
-    maps:update_with(A, fun (Acc) -> [B | Acc] end, [B], group(Rest)).
+group([{A, B} | Rest]) -> maps:update_with(A, fun(Acc) -> [B | Acc] end, [B], group(Rest)).
 
 apply_n(0, _, V) -> V;
 apply_n(N, F, V) when is_integer(N), N > 0 -> apply_n(N - 1, F, F(V)).
