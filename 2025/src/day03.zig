a77b235e0c492740172154f1da5c716bd642eccb
Erik Mattfolk <erik.mattfolk@gmail.com>
2025-12-06 23:25:22 +0100

    Day 3

---
+++ 2025/src/day03.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+const lib = @import("lib.zig");
+
+fn joltage(line: []const u8, res: u64, toGo: usize) u64 {
+    if (toGo == 0) {
+        return res;
+    }
+    var i: usize = 0;
+    var maxc = line[0];
+    var maxi: usize = 0;
+    for (line[0 .. line.len - toGo + 1]) |c| {
+        if (c > maxc) {
+            maxc = c;
+            maxi = i;
+        }
+        i += 1;
+    }
+    return joltage(line[maxi + 1 ..], res * 10 + @as(u64, maxc - '0'), toGo - 1);
+}
+
+pub fn solve(input: []const u8) !struct { []u8, []u8 } {
+    var ans1: u64 = 0;
+    var ans2: u64 = 0;
+    var parsed = lib.lines(input);
+    while (parsed.next()) |line| {
+        ans1 += joltage(line, 0, 2);
+        ans2 += joltage(line, 0, 12);
+    }
+    return .{ try lib.intToString(@intCast(ans1)), try lib.intToString(@intCast(ans2)) };
+}
--- 2025/src/main.zig
+++ 2025/src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const lib = @import("lib.zig");
 const day01 = @import("day01.zig");
 const day02 = @import("day02.zig");
+const day03 = @import("day03.zig");
 
 pub fn main() !void {
     var buf = try lib.readFile("input/day01.txt");
@@ -10,4 +11,7 @@ pub fn main() !void {
     buf = try lib.readFile("input/day02.txt");
     ans1, ans2 = try day02.solve(buf);
     std.debug.print("Day  2: {s}, {s}\n", .{ ans1, ans2 });
+    buf = try lib.readFile("input/day03.txt");
+    ans1, ans2 = try day03.solve(buf);
+    std.debug.print("Day  3: {s}, {s}\n", .{ ans1, ans2 });
 }
