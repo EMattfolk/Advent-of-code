e9767df2c48062956ba665e93711b12c98a5c863
Erik Mattfolk <erik.mattfolk@gmail.com>
2025-12-11 23:00:22 +0100

    Paralelize

--- 2025/src/main.zig
+++ 2025/src/main.zig
@@ -7,27 +7,35 @@ const day03 = @import("day03.zig");
 const day04 = @import("day04.zig");
 const day05 = @import("day05.zig");
 
+var res: [12]struct { []const u8, []const u8 } = undefined;
+
+fn runDay(day: usize, f: anytype, input: []const u8) !void {
+    res[day - 1] = try f(input);
+}
+
 pub fn main() !void {
     var buf: []const u8 = "";
-    var ans1: []const u8 = "";
-    var ans2: []const u8 = "";
+
     buf = try lib.readFile("input/day01.txt");
-    ans1, ans2 = try day01.solve(buf);
-    std.debug.print("Day  1: {s}, {s}\n", .{ ans1, ans2 });
-
+    var t1 = try std.Thread.spawn(.{}, runDay, .{ 1, day01.solve, buf });
     buf = try lib.readFile("input/day02.txt");
-    ans1, ans2 = try day02.solve(buf);
-    std.debug.print("Day  2: {s}, {s}\n", .{ ans1, ans2 });
-
+    var t2 = try std.Thread.spawn(.{}, runDay, .{ 2, day02.solve, buf });
     buf = try lib.readFile("input/day03.txt");
-    ans1, ans2 = try day03.solve(buf);
-    std.debug.print("Day  3: {s}, {s}\n", .{ ans1, ans2 });
-
+    var t3 = try std.Thread.spawn(.{}, runDay, .{ 3, day03.solve, buf });
     buf = try lib.readFile("input/day04.txt");
-    ans1, ans2 = try day04.solve(buf);
-    std.debug.print("Day  4: {s}, {s}\n", .{ ans1, ans2 });
-
+    var t4 = try std.Thread.spawn(.{}, runDay, .{ 4, day04.solve, buf });
     buf = try lib.readFile("input/day05.txt");
-    ans1, ans2 = try day05.solve(buf);
-    std.debug.print("Day  5: {s}, {s}\n", .{ ans1, ans2 });
+    var t5 = try std.Thread.spawn(.{}, runDay, .{ 5, day05.solve, buf });
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+
+    std.debug.print("Day  1: {s}, {s}\n", res[1 - 1]);
+    std.debug.print("Day  2: {s}, {s}\n", res[2 - 1]);
+    std.debug.print("Day  3: {s}, {s}\n", res[3 - 1]);
+    std.debug.print("Day  4: {s}, {s}\n", res[4 - 1]);
+    std.debug.print("Day  5: {s}, {s}\n", res[5 - 1]);
 }
