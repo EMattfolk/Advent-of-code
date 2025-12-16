2e12ddeb789926c93c11258301dc8ee6358401fc
Erik Mattfolk <erik.mattfolk@gmail.com>
2025-12-16 22:47:41 +0100

    Day 11

--- 2025/src/day11.zig
+++ 2025/src/day11.zig
@@ -5,32 +5,53 @@ pub fn solve(input: []const u8) struct { []u8, []u8 } {
     return solve_impl(input) catch .{ @constCast("error"), @constCast("error") };
 }
 
-fn all_paths(paths: *std.hash_map.AutoHashMap([3]u8, [][3]u8), node: [3]u8) i64 {
-    if (std.mem.eql(u8, &node, @constCast("out"))) {
+fn all_paths(paths: *std.hash_map.AutoHashMap([3]u8, [][3]u8), goal: [3]u8, node: [3]u8) i64 {
+    if (std.mem.eql(u8, &node, &goal)) {
         return 1;
     }
+
     var res: i64 = 0;
-    for (paths.get(node).?) |child| {
-        res += all_paths(paths, child);
+    if (paths.get(node)) |slice| {
+        for (slice) |child| {
+            res += all_paths(paths, goal, child);
+        }
+    }
+    return res;
+}
+
+fn can_reach(paths: *std.hash_map.AutoHashMap([3]u8, [][3]u8), visited: *std.hash_map.AutoHashMap([3]u8, void), goal: [3]u8, node: [3]u8) !bool {
+    if (std.mem.eql(u8, &node, &goal)) {
+        return true;
+    }
+    if (!visited.contains(node)) {
+        try visited.put(node, {});
+    } else {
+        return false;
+    }
+    var res: bool = false;
+    if (paths.get(node)) |slice| {
+        for (slice) |child| {
+            res = res or try can_reach(paths, visited, goal, child);
+        }
     }
     return res;
 }
 
 const State = struct { count: i64, dac: bool, fft: bool };
 
-fn combine(a: State, b: State) State {
-    if (a.dac == b.dac and a.fft == b.fft) {
-        return .{ .count = a.count + b.count, .dac = a.dac, .fft = a.fft };
-    } else {
-        const sa: u8 = if (a.dac and a.fft) 2 else if (a.dac or a.fft) 1 else 0;
-        const sb: u8 = if (b.dac and b.fft) 2 else if (b.dac or b.fft) 1 else 0;
-        if (sa > sb) {
-            return a;
-        } else {
-            return b;
-        }
-    }
-}
+// fn combine(a: State, b: State) State {
+//     if (a.dac == b.dac and a.fft == b.fft) {
+//         return .{ .count = a.count + b.count, .dac = a.dac, .fft = a.fft };
+//     } else {
+//         const sa: u8 = if (a.dac and a.fft) 2 else if (a.dac or a.fft) 1 else 0;
+//         const sb: u8 = if (b.dac and b.fft) 2 else if (b.dac or b.fft) 1 else 0;
+//         if (sa > sb) {
+//             return a;
+//         } else {
+//             return b;
+//         }
+//     }
+// }
 
 fn solve_impl(input: []const u8) !struct { []u8, []u8 } {
     var ans1: i64 = 0;
@@ -48,64 +69,102 @@ fn solve_impl(input: []const u8) !struct { []u8, []u8 } {
         try paths.put(parent[0..3].*, children.items);
     }
 
-    ans1 = all_paths(&paths, @constCast("you").*);
+    ans1 = all_paths(&paths, @constCast("out").*, @constCast("you").*);
 
-    var visited = std.AutoHashMap([3]u8, State).init(lib.a);
-    try visited.put(@constCast("svr").*, .{ .count = 1, .dac = false, .fft = false });
-    defer visited.deinit();
-    var toVisit: std.ArrayList([3]u8) = .empty;
-    defer toVisit.deinit(lib.a);
-    var newToVisit: std.ArrayList([3]u8) = .empty;
-    defer newToVisit.deinit(lib.a);
-    try toVisit.append(lib.a, @constCast("svr").*);
-    var steps: i64 = 0;
-    while (toVisit.items.len > 0) : (steps += 1) {
-        for (toVisit.items) |node| {
-            if (std.mem.eql(u8, &node, @constCast("out"))) {
-                continue;
-            }
-            if (std.mem.eql(u8, &node, @constCast("fft"))) {
-                var old = visited.get(node).?;
-                old.fft = true;
-                try visited.put(node, old);
-            }
-            if (std.mem.eql(u8, &node, @constCast("dac"))) {
-                var old = visited.get(node).?;
-                old.dac = true;
-                try visited.put(node, old);
-            }
+    // var visited = std.AutoHashMap([3]u8, State).init(lib.a);
+    // try visited.put(@constCast("svr").*, .{ .count = 1, .dac = false, .fft = false });
+    // defer visited.deinit();
+    // var toVisit: std.ArrayList([3]u8) = .empty;
+    // defer toVisit.deinit(lib.a);
+    // var newToVisit: std.ArrayList([3]u8) = .empty;
+    // defer newToVisit.deinit(lib.a);
+    // try toVisit.append(lib.a, @constCast("svr").*);
+    // var steps: i64 = 0;
+    // while (toVisit.items.len > 0) : (steps += 1) {
+    //     for (toVisit.items) |node| {
+    //         if (std.mem.eql(u8, &node, @constCast("out"))) {
+    //             continue;
+    //         }
+    //         if (std.mem.eql(u8, &node, @constCast("fft"))) {
+    //             var old = visited.get(node).?;
+    //             old.fft = true;
+    //             try visited.put(node, old);
+    //         }
+    //         if (std.mem.eql(u8, &node, @constCast("dac"))) {
+    //             var old = visited.get(node).?;
+    //             old.dac = true;
+    //             try visited.put(node, old);
+    //         }
 
-            for (paths.get(node).?) |child| {
-                if (!visited.contains(child)) {
-                    try visited.put(child, visited.get(node).?);
-                    try newToVisit.append(lib.a, child);
-                } else {
-                    const a = visited.get(node).?;
-                    const b = visited.get(child).?;
-                    try visited.put(child, combine(a, b));
+    //         for (paths.get(node).?) |child| {
+    //             if (!visited.contains(child)) {
+    //                 try visited.put(child, visited.get(node).?);
+    //                 try newToVisit.append(lib.a, child);
+    //             } else {
+    //                 const a = visited.get(node).?;
+    //                 const b = visited.get(child).?;
+    //                 try visited.put(child, combine(a, b));
 
-                    var shouldInsert = true;
-                    for (newToVisit.items) |new| {
-                        if (std.mem.eql(u8, &child, &new)) {
-                            shouldInsert = false;
-                            break;
-                        }
-                    }
-                    if (shouldInsert) {
-                        try newToVisit.append(lib.a, child);
-                    }
-                }
-            }
-        }
-        std.mem.swap(std.ArrayList([3]u8), &toVisit, &newToVisit);
-        newToVisit.shrinkRetainingCapacity(0);
-    }
+    //                 var shouldInsert = true;
+    //                 for (newToVisit.items) |new| {
+    //                     if (std.mem.eql(u8, &child, &new)) {
+    //                         shouldInsert = false;
+    //                         break;
+    //                     }
+    //                 }
+    //                 if (shouldInsert) {
+    //                     try newToVisit.append(lib.a, child);
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     std.mem.swap(std.ArrayList([3]u8), &toVisit, &newToVisit);
+    //     newToVisit.shrinkRetainingCapacity(0);
+    // }
 
-    ans2 = visited.get(@constCast("out").*).?.count;
+    // ans2 = visited.get(@constCast("out").*).?.count;
 
     // 46366925751047616 high
+    // 520476725037672
 
-    std.debug.print("{}\n", .{visited.get(@constCast("out").*).?});
+    // std.debug.print("{}\n", .{visited.get(@constCast("out").*).?});
+
+    var visited = std.AutoHashMap([3]u8, void).init(lib.a);
+    var toRemove: std.ArrayList([3]u8) = .empty;
+    var it2 = paths.keyIterator();
+    while (it2.next()) |node| {
+        if (!try can_reach(&paths, &visited, @constCast("fft").*, node.*)) {
+            try toRemove.append(lib.a, node.*);
+        }
+        visited.clearRetainingCapacity();
+    }
+
+    var pathsToFft: std.hash_map.AutoHashMap([3]u8, [][3]u8) = try paths.clone();
+    for (toRemove.items) |r| {
+        _ = pathsToFft.remove(r);
+    }
+
+    // std.debug.print("{}\n", .{toRemove.items.len});
+
+    ans2 = all_paths(&pathsToFft, @constCast("fft").*, @constCast("svr").*);
+
+    toRemove.shrinkRetainingCapacity(0);
+
+    it2 = paths.keyIterator();
+    while (it2.next()) |node| {
+        if (!try can_reach(&paths, &visited, @constCast("dac").*, node.*)) {
+            try toRemove.append(lib.a, node.*);
+        }
+        visited.clearRetainingCapacity();
+    }
+
+    var pathsToDac: std.hash_map.AutoHashMap([3]u8, [][3]u8) = try paths.clone();
+    for (toRemove.items) |r| {
+        _ = pathsToDac.remove(r);
+    }
+
+    ans2 *= all_paths(&pathsToDac, @constCast("dac").*, @constCast("fft").*);
+    ans2 *= all_paths(&paths, @constCast("out").*, @constCast("dac").*);
 
     return .{ try lib.intToString(ans1), try lib.intToString(ans2) };
 }
